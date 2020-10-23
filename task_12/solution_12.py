98aa3d021cce6ca3e0c6260ded12fbfb718045fa
schneeewittchen <72072516+schneeewittchen@users.noreply.github.com>
2020-10-23 22:59:21 +0300

    Update solution_12.py

--- task_12/solution_12.py
+++ task_12/solution_12.py
@@ -1,1 +1,9 @@
-# your code here
+teststring=str(input())
+occurrences=0
+for letter in teststring:
+    double=letter*2
+    if teststring.count(double)==2:
+        occurrences=occurrences+1
+if occurrences>=2 : occurred_twice='Yes'   
+else: occurred_twice='No'
+print(occurred_twice)
