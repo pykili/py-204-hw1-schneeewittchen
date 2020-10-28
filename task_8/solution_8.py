f131622a52fa74171ad58e26b26b59c23f733de0
schneeewittchen <72072516+schneeewittchen@users.noreply.github.com>
2020-10-28 23:58:23 +0300

    Update solution_8.py

--- task_8/solution_8.py
+++ task_8/solution_8.py
@@ -1,1 +1,15 @@
-# your code here
+n = int(input())
+sum = 0
+denominator = 0
+for i in range(1, n+1):
+  user_input = input()
+  if user_input == '' : 
+    pass
+  elif user_input == '0' :
+    pass
+  else :
+    number = int(user_input)
+    sum = sum + number
+    denominator = denominator + 1
+average = sum/denominator
+print('Average is', average)
