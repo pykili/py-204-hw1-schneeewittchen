3ad0f22df7b4888a83a4a3a3840ef461846c2a68
schneeewittchen <72072516+schneeewittchen@users.noreply.github.com>
2020-10-28 21:56:04 +0300

    Update solution_1.py

--- task_1/solution_1.py
+++ task_1/solution_1.py
@@ -1,3 +1,20 @@
-print('Введите последовательность цифр')
 a = input()
-print('Самая большая циФра', max(a))
+if '9' in a:
+  print('9')
+elif '8' in a :
+  print('8')
+elif '7' in a :
+  print('7')
+elif '6' in a :
+  print('6')
+elif '5' in a :
+  print('5')
+elif '4' in a :
+  print('4')
+elif '3' in a :
+  print('3')
+elif '2' in a :
+  print('2')
+elif '1' in a :
+  print('1')
+else : print('0')
