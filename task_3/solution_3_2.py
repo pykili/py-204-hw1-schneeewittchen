9e441cdb8007e833bb211c059f3afc7bf9dcd4f9
schneeewittchen <72072516+schneeewittchen@users.noreply.github.com>
2020-10-28 23:07:08 +0300

    Update solution_3_2.py

--- task_3/solution_3_2.py
+++ task_3/solution_3_2.py
@@ -1,6 +1,16 @@
-# your code here
-for smth in 'a'*10:
-    # your code here
-    user_input = input()
-# your code here
-print(alphabet)
+string1 = input()
+string2 = input()
+string3 = input()
+string4 = input()
+string5 = input()
+string6 = input()
+string7 = input()
+string8 = input()
+string9 = input()
+string10 = input()
+big_string = string1 + string2 + string3 + string4 + string5 + string6 + string7 + string8 + string9 + string10
+alphabet = ''
+for letter in big_string:
+  if letter not in alphabet :
+    alphabet = alphabet + letter
+print('the alphabet is', alphabet)
