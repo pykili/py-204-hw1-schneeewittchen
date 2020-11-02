4712108209e102a34c7b24ed2d787cb7236e2cc6
schneeewittchen <72072516+schneeewittchen@users.noreply.github.com>
2020-11-03 02:17:16 +0300

    Update solution_4_2.py

--- task_4/solution_4_2.py
+++ task_4/solution_4_2.py
@@ -1,6 +1,30 @@
-# your code here
-for smth in 'a'*10:
-    user_input = input()
-    # your code here
-    if my_cool_condition == True:
-        print(form, lemma)
+for i in range(10):
+  form = ''
+  lemma = ''
+  id = ''
+  user_input = input()
+  if user_input == '' : 
+    pass
+  elif user_input[0] == '#' :
+    pass
+  else :
+    for letter in user_input :
+      id = id + letter
+      if letter == '\t' :
+        break
+    id_length = len(id)
+    user_input = user_input[id_length:]
+    for letter in user_input :
+      form = form + letter
+      if letter == '\t' :
+        break
+    form_length = len(form)
+    user_input = user_input[form_length:]
+    for letter in user_input :
+      lemma = lemma + letter
+      if letter == '\t' :
+        break
+    lemma_length = len(lemma) - 1
+    new_form = form[:lemma_length] + '\t'
+    if (form != lemma and lemma != new_form) :
+      print(form, lemma)
